6dc0ad08ed6add00a7ccc4c65de5a8b4fa8bfb03
Mehran <mehranbhat2001@gmail.com>
2025-02-11 16:28:28 +0530

    Models completed

---
+++ models/userModel.js
@@ -0,0 +1,42 @@
+const mongoose = require('mongoose');
+const userSchema = mongoose.Schema(
+  {
+    email: {
+      typeof: String,
+      required: [true, 'email required'],
+      trim: true,
+      unique: [true, 'email must be unique'],
+      lowercase: true,
+    },
+    password: {
+      typeof: String,
+      required: [true, 'password required'],
+      trim: true,
+      select: false,
+    },
+    verified: {
+      type: Boolean,
+      default: false,
+    },
+    verificationCode: {
+      type: String,
+      select: false,
+    },
+    verificationCodeValidation: {
+      type: Number,
+      select: false,
+    },
+    forgotPasscode: {
+      type: String,
+      select: false,
+    },
+    forgotPasscodeValidation: {
+      type: Number,
+      select: false,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+module.exports = mongoose.model('User', userSchema);
