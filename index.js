98ad825ff8bea34054b82865537f870f0194ed0e
Mehran <mehranbhat2001@gmail.com>
2025-02-11 16:07:15 +0530

    DataBase setup and folders

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
-console.log('helloo');
+const express = require('express');
+const app = express();
+const cors = require('cors');
+const cookieParser = require('cookie-parser');
+const helmet = require('helmet');
+const mongoose = require('mongoose');
+
+app.use(cors());
+app.use(cookieParser());
+app.use(helmet());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => {
+    console.log('dataBase connected');
+  })
+  .catch(err => {
+    console.log(err);
+  });
+app.get('/', (req, res) => {
+  res.json({ messagae: 'hii how are you' });
+});
+
+app.listen(process.env.PORT, () => {
+  console.log('server started');
+});
